b8a2b8801f327298088b631258192c409fbeb760
David Jacobs <david.jacobs@tangramflex.com>
2019-01-16 15:38:20 -0500

    Add find and delete to client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -5,34 +5,74 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/djacobs24/go-twirp-example/rpc/haberdasher"
+	"github.com/djacobs24/go-twirp-example/rpc/hats"
+	"github.com/segmentio/ksuid"
 )
 
 func main() {
-
 	// Create protobuf client
-	client := haberdasher.NewHaberdasherProtobufClient("http://localhost:5555", &http.Client{})
+	client := hats.NewHatsProtobufClient("http://localhost:5555", &http.Client{})
+	createHat(client)
+	findHat(client)
+	deleteHat(client)
+}
 
+func createHat(client hats.Hats) {
 	// Construct request
-	request := &haberdasher.CreateHatRequest{
+	request := &hats.CreateHatRequest{
 		Name:  "Baseball Cap",
-		Size:  &haberdasher.Size{Inches: 12},
+		Size:  &hats.Size{Inches: 22},
 		Color: "Black",
 	}
-
 	// Create hat
 	hat, err := client.CreateHat(context.Background(), request)
 	if err != nil {
-		fmt.Printf("ERROR: %v", err)
+		fmt.Printf("Create Hat Error: %v", err)
 		return
 	}
-
 	// Parse hat data
 	id := hat.ID
 	name := hat.Name
 	inches := hat.Size.Inches
 	color := hat.Color
-
 	// Print hat data
 	fmt.Printf("\nCreated Hat:\nID: %s\nName: %s\nSize: %d\nColor: %s\n", id, name, inches, color)
+}
+
+func findHat(client hats.Hats) {
+	// Generate id
+	id := ksuid.New().String()
+	// Construct request
+	request := &hats.FindHatRequest{
+		ID: id,
+	}
+	// Find hat
+	resp, err := client.FindHat(context.Background(), request)
+	if err != nil {
+		fmt.Printf("Find Hat Error: %v", err)
+		return
+	}
+	// Parse hat data
+	name := resp.Name
+	inches := resp.Size.Inches
+	color := resp.Color
+	// Print hat data
+	fmt.Printf("\nFound Hat:\nID: %s\nName: %s\nSize: %d\nColor: %s\n", id, name, inches, color)
+}
+
+func deleteHat(client hats.Hats) {
+	// Generate id
+	id := ksuid.New().String()
+	// Construct request
+	request := &hats.DeleteHatRequest{
+		ID: id,
+	}
+	// Delete hat
+	_, err := client.DeleteHat(context.Background(), request)
+	if err != nil {
+		fmt.Printf("Delete Hat Error: %v", err)
+		return
+	}
+	// Print hat data
+	fmt.Printf("\nDeleted Hat:\nID: %s\n", id)
 }
